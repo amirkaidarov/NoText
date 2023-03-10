457a2c277af3120583e5245850cacd6ae93c9840
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-03-10 17:24:58 -0500

    Initial Commit

---
+++ NoText/NoTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoTextApp.swift
+//  NoText
+//
+//  Created by Амир Кайдаров on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NoTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
